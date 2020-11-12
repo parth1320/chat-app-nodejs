6d8b7b0f442ed4095bcbd37252410fc666dc0fcd
parth1320 <51327821+parth1320@users.noreply.github.com>
2020-11-12 12:45:25 +0530

    Init

---
+++ src/utils/users.js
@@ -0,0 +1,57 @@
+const users = []
+
+const addUser = ({ id, username, room }) => {
+    //clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    //validata the data
+
+    if(!username || !room) {
+        return {
+            error: 'username and room are required!' 
+        }
+    }
+
+    //check for existing user
+
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    //store user
+
+    const user = {id, username, room}
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUserInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUserInRoom
+}
